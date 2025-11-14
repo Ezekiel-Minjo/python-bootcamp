a3840cfd2d0998c920d9eb4132f58ba6ef55a211
minjo <ezekielminjo4@gmail.com>
2025-11-15 00:03:01 +0300

    classes and objects in python

--- class_object.py
+++ class_object.py
@@ -1,13 +1,14 @@
-# creating a class
-class Employer:
-  def __init__(self, name, industry):
-    self.name = name # attribute
-    self.industry = industry # attribute
+class Person:
+  def __init__(self, name, age):
+    # This is the contructor method that is called when creating a new Person object
+    # It takes two parameters, name, and age, and initializes them as attributes of the objects
+    self.name = name
+    self.age = age
 
-  def display_info(self): # method
-    print(f"Employer: {self.name} - {self.industry}")
-    
-# Creating an object
-employer_one = Employer("Tpoint Tech", "Education")
-employer_one.display_info()
+  def greet(self):
+    # This is a method of the Person class that prints a greeting message
+    print("Hello, my name is " + self.name)  
 
+# creating a new instance of the Person class and assign it to the variable person1
+person1 = Person("Ayan", 25)
+person1.greet()
